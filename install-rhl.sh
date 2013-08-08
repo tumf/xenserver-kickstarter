5ab172d4e193e18fd5bbe0fea1b72308a2c26b7e
Yoshihiro TAKAHARA <y.takahara@gmail.com>
2013-08-08 18:42:44 +0900

    remove xenserver-automator

--- install-rhl.sh
+++ install-rhl.sh
@@ -1,15 +1,10 @@
 #!/bin/sh
 
 # Install scripts
-install -m 755 usr/sbin/xe-set-authorized-keys /usr/sbin/xe-set-authorized-keys
-install -m 755 usr/sbin/xe-set-hostname /usr/sbin/xe-set-hostname
-install -m 755 usr/sbin/xe-set-network-rpm /usr/sbin/xe-set-network
-
 install -m 755 etc/init.d/kickstart /etc/init.d/kickstart
-install -m 755 etc/init.d/xe-automater /etc/init.d/xe-automater
 
 # set service
 /sbin/chkconfig kickstart on
-/sbin/chkconfig xe-automater off
+
 
 
